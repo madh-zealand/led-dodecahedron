a34b94b1ab172c16cac3d3483e3ad274559d0aca
Martin Dilling-Hansen <martindilling@gmail.com>
2025-03-27 20:42:40 +0100

    Log full message event

--- led-dodecahedron.js
+++ led-dodecahedron.js
@@ -100,10 +100,10 @@ edges.forEach(([v1, v2]) => {
 
 parent.postMessage({ app: 'wokwi', command: 'listen', version: 1 }, 'https://wokwi.com');
 
-window.addEventListener('message', ({ data }) => {
-  console.log({data: data});
-  if (data.neopixels) {
-    const { neopixels } = data;
+window.addEventListener('message', (event) => {
+  console.log({event: event});
+  if (event.data.neopixels) {
+    const { neopixels } = event.data;
     for (let i = 0; i < neopixels.length; i++) {
       const value = neopixels[i];
       const b = value & 0xff;
